218e9c0e40d38b8c3ba515b92056c89fecd81be4
Tomas Krchnak <tomas.krchnak@millennium.sk>
2018-06-13 21:38:47 +0200

    wip

--- ImdbMoviesConsoleApp/Program.cs
+++ ImdbMoviesConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -10,34 +11,92 @@ namespace ImdbMoviesConsoleApp
 {
     class Program
     {
-        const string apiKey = "3bc42aaa";
-        const string requestUrl = "http://www.omdbapi.com/";
-        const string urlParameters = "?apikey=3bc42aaa&i=tt4154756";
+        const string RequestUrl = "http://www.omdbapi.com/";
+        const string UrlParameters = "?apikey=3bc42aaa&type=movie&i=tt";
 
         static void Main(string[] args)
         {
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(requestUrl);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            List<Movie> movies = new List<Movie>();
 
+            int imdbIdFrom = 4154756;
+
+            for (int i = imdbIdFrom; i >= (imdbIdFrom - 100); i--)
+            {
+                string imdbIdNBumericPart = i.ToString();
+                //Console.WriteLine(imdbIdNBumericPart);
+                Movie movie = GetMovieDataByImdbId(imdbIdNBumericPart);
+                if (movie != null)
+                {
+                    movies.Add(movie);
+                }
+            }
+            
+            sw.Stop();
+            Console.WriteLine($"Get movies - duration: {sw.ElapsedMilliseconds} ms");
+
+            sw.Restart();
+            PrintMovies(movies);
+            sw.Stop();
+            Console.WriteLine($"Print movies - duration: {sw.ElapsedMilliseconds} ms");
+
+            Console.ReadLine();
+        }
+
+        private static void PrintMovies(List<Movie> movies)
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            foreach (var movie in movies)
+            {
+                sb.AppendLine(movie.ToString());
+            }
+
+            Console.WriteLine(sb);
+        }
+
+        private static Movie GetMovieDataByImdbId(string imdbIdNBumericPart)
+        {
+            HttpClient client = new HttpClient();
+            client.Timeout = new TimeSpan(0, 0, 5);
+            string urlParameters = UrlParameters + imdbIdNBumericPart;
+            client.BaseAddress = new Uri(RequestUrl);
             // Add an Accept header for JSON format.
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
             // List data response.
-            HttpResponseMessage response = client.GetAsync(urlParameters).Result;  // Blocking call!
+            HttpResponseMessage response = null;
+            try
+            {
+                response = client.GetAsync(urlParameters).Result;  // Blocking call!
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            if (response == null)
+            {
+                return null;
+            }
+
             if (response.IsSuccessStatusCode)
             {
                 // Parse the response body. Blocking!
                 var moveResponse = response.Content.ReadAsAsync<MovieResponse>().Result;
+                if (String.IsNullOrEmpty(moveResponse.imdbID) || moveResponse.imdbID.Equals("N/A"))
+                {
+                    return null;
+                }
                 var movie = new Movie(moveResponse);
-                Console.WriteLine(movie.ToString());
+                return movie;
             }
             else
             {
                 Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
             }
-
-            Console.WriteLine("Done");
-            Console.ReadLine();
+            return null;
         }
     }
 
