febb7c68776f5ee84fe4aefb6f43017e22ad9521
Tomas Krchnak <tomas.krchnak@millennium.sk>
2018-06-15 18:03:15 +0200

    parellel wip

--- ImdbMoviesConsoleApp/Program.cs
+++ ImdbMoviesConsoleApp/Program.cs
@@ -16,28 +16,44 @@ namespace ImdbMoviesConsoleApp
         {
             Stopwatch sw = new Stopwatch();
             sw.Start();
-            OmdbApiManager omdbApiManager = new OmdbApiManager();
-            MovieExporter movieExporter = new MovieExporter();
 
             List<Movie> movies = new List<Movie>();
+            MovieExporter movieExporter = new MovieExporter();
 
             int imdbIdFrom = 4154756;
-            int counter = 0;
+            int movieCount = 500;
 
-            while (counter <= 10)
-            {
-                string imdbIdNBumericPart = imdbIdFrom.ToString();
-                Movie movie = omdbApiManager.GetMovieDataByImdbId(imdbIdNBumericPart);
-                if (movie != null)
-                {
-                    movies.Add(movie);
-                    counter++;
-                }
-                imdbIdFrom--;
-            }
+            Task<List<Movie>> taskA = Task<List<Movie>>.Run(() => GetMovies(imdbIdFrom, movieCount));
+            Task<List<Movie>> taskB = Task<List<Movie>>.Run(() => GetMovies((imdbIdFrom - movieCount), movieCount));
+            Task<List<Movie>> taskC = Task<List<Movie>>.Run(() => GetMovies((imdbIdFrom - movieCount), movieCount));
+            Task<List<Movie>> taskD = Task<List<Movie>>.Run(() => GetMovies((imdbIdFrom - movieCount), movieCount));
+            Task<List<Movie>> taskE = Task<List<Movie>>.Run(() => GetMovies((imdbIdFrom - movieCount), movieCount));
 
-            sw.Stop();
-            Logger.WriteLog($"Get movies - duration: {sw.ElapsedMilliseconds} ms");
+
+            // movies.AddRange(GetMovies(imdbIdFrom, movieCount));
+
+            taskA.Wait();
+            taskB.Wait();
+            taskC.Wait();
+            taskD.Wait();
+            taskE.Wait();
+
+            Logger.WriteLog("taskA.Result count: "  + taskA.Result.Count);
+            movies.AddRange(taskA.Result);
+
+            Logger.WriteLog("taskB.Result count: " + taskB.Result.Count);
+            movies.AddRange(taskB.Result);
+
+            Logger.WriteLog("taskC.Result count: " + taskC.Result.Count);
+            movies.AddRange(taskC.Result);
+
+            Logger.WriteLog("taskD.Result count: " + taskD.Result.Count);
+            movies.AddRange(taskD.Result);
+
+            Logger.WriteLog("taskE.Result count: " + taskE.Result.Count);
+            movies.AddRange(taskE.Result);
+
+
 
             sw.Restart();
             string moviesInfo = PrintMovies(movies);
@@ -48,7 +64,34 @@ namespace ImdbMoviesConsoleApp
             movieExporter.ExportMoviesToCsv(moviesInfo);
             sw.Stop();
             Logger.WriteLog($"Export movies to CSV - duration: {sw.ElapsedMilliseconds} ms");
-        }        
+        }       
+        
+        private static List<Movie> GetMovies(int imdbIdFrom, int movieCount)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            List<Movie> movies = new List<Movie>();
+
+            OmdbApiManager omdbApiManager = new OmdbApiManager();       
+            int counter = 0;
+
+            while (counter <= movieCount) //TODO: prerobit na paralelne volanie
+            {
+                string imdbIdNBumericPart = imdbIdFrom.ToString();
+                Movie movie = omdbApiManager.GetMovieDataByImdbId(imdbIdNBumericPart);
+                if (movie != null)
+                {
+                    movies.Add(movie);
+                }
+                counter++;
+                imdbIdFrom--;
+            }
+
+            sw.Stop();
+            Logger.WriteLog($"Get movies - duration: {sw.ElapsedMilliseconds} ms");
+
+            return movies;
+        }
 
         private static string PrintMovies(List<Movie> movies)
         {
