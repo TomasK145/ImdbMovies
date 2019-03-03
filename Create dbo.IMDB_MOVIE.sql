6cec5cf8333893f2aa791b9933f3439760d08292
Tomas Krchnak <tomas.krchnak@millennium.sk>
2019-03-03 20:34:20 +0100

    wip

--- Create dbo.IMDB_MOVIE.sql
+++ Create dbo.IMDB_MOVIE.sql
@@ -1,7 +1,7 @@
 ﻿USE [ImdbMoviesDb]
 GO
 select INFO_MESSAGE, count(*) from [dbo].[IMDB_MOVIE] with (nolock) group by INFO_MESSAGE
---> 1540616 --> 1596723 --> 1601611 --> 1607194
+--> 1540616 --> 1596723 --> 1601611 --> 1607194 --> 1695468
 --> 3,9m
  
 select min(mm.IMDB_ID_NUM), max(mm.IMDB_ID_NUM)
--- ImdbMoviesConsoleApp/Program.cs
+++ ImdbMoviesConsoleApp/Program.cs
@@ -51,8 +51,8 @@ namespace ImdbMoviesConsoleApp
             {
                 Console.WriteLine("Prebieha...");
                 //posledne spracovane: takeCount:  // skipCount:
-                int takeCount = 100000;
-                int skipCount = 4400000; //zvysok o tie kt boli checknute
+                int takeCount = 500000;
+                int skipCount = 8800000; //zvysok o tie kt boli checknute
 
                 List<int> failedMoviesIds = dbProcessor.GetNotExistingMovieIds(takeCount, skipCount);
                 GetFailedMoviesFromImdbToDatabase(failedMoviesIds);
